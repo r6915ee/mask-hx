5aa91bd7986a4cd8ef04e432dd44a8604d05057c
r6915ee <astergthefourth@gmail.com>
2025-08-09 20:08:21 -0700

    feat: add fetcher::haxe_path
    `fetcher::is_haxe_version_installed` now depends on the function above.

--- libmask/src/fetcher.rs
+++ libmask/src/fetcher.rs
@@ -3,6 +3,33 @@ use std::path::PathBuf;
 
 use crate::config;
 
+/// Simply constructs a path to a [Haxe](https://haxe.org).
+///
+/// Paths to [Haxe](https://haxe.org) versions are passed to
+/// this function. Do note that this does not check if they
+/// actually exist; for this purpose, [is_haxe_version_installed]
+/// uses this function to simply construct the path, and also
+/// checks the existence of the path.
+pub fn haxe_path(version: &str) -> Result<PathBuf, Error> {
+    let home: Option<PathBuf> = std::env::home_dir();
+
+    match home {
+        Some(buf_val) => {
+            let mut buf: PathBuf = buf_val.clone();
+
+            buf.push(".haxe");
+            println!("{}", version);
+            buf.push(version);
+
+            Ok(buf)
+        }
+        None => Err(Error::new(
+            ErrorKind::NotFound,
+            "Home directory not accessible",
+        )),
+    }
+}
+
 /// Checks if a Haxe version is installed.
 ///
 /// Haxe versions are installed in the `~/.haxe/` directory, where
@@ -12,22 +39,28 @@ use crate::config;
 /// library is found under there, then [fetcher](..) will consider it to
 /// be valid.
 pub fn is_haxe_version_installed(version: &str) -> Result<bool, Error> {
-    let home: Option<PathBuf> = std::env::home_dir();
-
-    match home {
-        Some(buf_val) => {
-            let mut buf: PathBuf = buf_val.clone();
-
-            buf.push(".haxe");
-            buf.push(version);
-            buf.push("std");
-
-            buf.try_exists()
-        }
-        None => Err(Error::new(
-            ErrorKind::NotFound,
-            "Home directory not accessible",
-        )),
+    match haxe_path(version) {
+        Ok(path) => match path.try_exists() {
+            Ok(_) => {
+                let mut path = path;
+                path.push("std");
+                match path.try_exists() {
+                    Ok(exists) => Ok(exists),
+                    Err(_) => Err(Error::new(
+                        ErrorKind::NotFound,
+                        format!(
+                            "Standard library for Haxe version {} could not be found",
+                            version
+                        ),
+                    )),
+                }
+            }
+            Err(_) => Err(Error::new(
+                ErrorKind::NotFound,
+                format!("Haxe version {} could not be found", version),
+            )),
+        },
+        Err(e) => Err(e),
     }
 }
 
