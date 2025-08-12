596781cff4d1727d33b5fe686c4d63ffa13424eb
r6915ee <astergthefourth@gmail.com>
2025-08-11 22:09:41 -0700

    fix: add path env setting

--- libmask/src/interactive.rs
+++ libmask/src/interactive.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::io;
 use std::io::{Error, ErrorKind};
 use std::path::PathBuf;
@@ -31,6 +32,14 @@ pub fn exec(
             true => match fetcher::haxe_path(used_version.as_str()) {
                 Ok(buf) => {
                     let mut buf: PathBuf = buf;
+
+                    match env::var("PATH") {
+                        Ok(data) => unsafe {
+                            env::set_var("PATH", format!("{}:{}", buf.display(), data));
+                        },
+                        Err(_) => {}
+                    }
+
                     buf.push(prog.unwrap_or("haxe".to_string()));
 
                     let output: Output = Command::new(buf)
