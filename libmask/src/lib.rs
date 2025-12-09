76e8d10be40ad7b46ea7c26c3d296d8a2c7c0e77
r6915ee <astergthefourth@gmail.com>
2025-12-09 07:06:51 +0000

    docs: improve most of libmask api documentation

--- libmask/src/lib.rs
+++ libmask/src/lib.rs
@@ -1,17 +1,81 @@
-//! # libmask
+//! # `libmask`
 //!
-//! `libmask` aims to simplify [Haxe](https://haxe.org)
-//! development by providing version management capabilities.
+//! [Haxe]: https://haxe.org/
 //!
-//! [Haxe](https://haxe.org), unlike other toolchains and
-//! programming languages, lacks unified compatibility between
-//! versions; to work around this, complex systems need to
-//! be set up to actually use good version management.
+//! `libmask` aims to simplify [Haxe] development by providing version
+//! management capabilities.
 //!
-//! `libmask` aims to provide an interface to allow programs
-//! to do this easily. The de facto standard in programs that
-//! use this library is `mask-hx`, which is its parent project,
-//! but `libmask` is usable by anyone.
+//! [Haxe], unlike other toolchains, lacks unified compatibility between
+//! versions; even minor versions may create new non-backwards compatible
+//! syntax. To work around this, complex systems need to be set up to
+//! actually use good version management, and they are typically tedious to
+//! use. This means that there may be issues in the long term, such as slower
+//! work and much more trouble configuring multi-version setups.
+//!
+//! `libmask` aims to provide an interface to allow programs to perform
+//! transparent version management. The de facto standard in programs that use
+//! this library is `mask-hx`, which is its parent project, but `libmask` is
+//! usable by anyone.
+//!
+//! ## Description
+//!
+//! `libmask` grants an interface to handle [Haxe] versions through filesystem
+//! management and simple configuration files.
+//!
+//! ### Haxe Versions
+//!
+//! [Haxe] versions are provided through a simple [`HaxeVersion`] tuple struct.
+//! The structure provides an implementation focused on the access of the
+//! actual version directory.
+//!
+//! ### Configuration
+//!
+//! `libmask` uses a very simple configuration file format that contains only a
+//! version number string. For example, below would be a valid configuration,
+//! since it only contains a version number:
+//!
+//! ```c
+//! 4.2.5
+//! ```
+//!
+//! Newlines are always stripped when reading files.
+//!
+//! Configuration files are usable through the [`Config`] tuple struct, which
+//! wraps a [`HaxeVersion`] tuple struct as data and provides configuration
+//! file reading, writing, and parsing.
+//!
+//! ### Program Execution
+//!
+//! All programs under a valid [Haxe] version directory can be executed using
+//! the [`haxe_exec`] method in the root module. This method modifies the
+//! environment the child process is in, ensuring that further child processes
+//! will also make use of the programs, avoiding complications with system
+//! packages.
+//!
+//! ## Usage
+//!
+//! The following is a sample of working with `libmask`:
+//!
+//! ```rust
+//! use libmask::*;
+//!
+//! # fn main() {
+//! // The first argument in the configuration constructor can accept a
+//! // custom configuration file path by wrapping it in an Option.
+//! let config: Config = match Config::new(None) {
+//!     Ok(data) => data,
+//!     // Although it's not recommended to construct configurations
+//!     // without performing any reading, the nature of tuple structs
+//!     // allows this kind of construction.
+//!     Err(_) => Config(HaxeVersion("4.2.5".into()))
+//! };
+//!
+//! match haxe_exec(vec!["--help".into()], config, Some("haxe".into())) {
+//!     Ok(_) => println!("Successfully ran Haxe compiler"),
+//!     Err(e) => println!("{}", e),
+//! }
+//! # }
+//! ```
 
 use std::env;
 use std::fs;
@@ -19,7 +83,7 @@ use std::io::{Error, ErrorKind};
 use std::path::{Path, PathBuf};
 use std::process::{Command, Output, Stdio};
 
-/// Basic structure that details a Haxe version.
+/// Basic structure that details a [Haxe](https://haxe.org/) version.
 pub struct HaxeVersion(pub String);
 
 impl HaxeVersion {
@@ -40,7 +104,7 @@ impl HaxeVersion {
         ))
     }
 
-    /// Gets a path to this Haxe version.
+    /// Gets a path to the current Haxe version.
     ///
     /// Do be aware that this method does **not** check whether or not the path
     /// is valid. Instead, you should use
@@ -150,11 +214,18 @@ impl Default for Config {
     }
 }
 
-/// Executes a specified Haxe-only program.
+/// Executes a specified program under a version directory.
 ///
-/// `libmask` will check ahead of time if the program specified is available;
-/// after all, the program must be available; otherwise, this method would
-/// simply not work.
+/// `libmask` will check ahead of time if the program specified is available as
+/// a safety precaution, and will otherwise fail if the program cannot be
+/// accessed.
+///
+/// Programs executed will have their `PATH` environment variable prepended
+/// with the [Haxe](https://haxe.org/) version directory the program is in.
+/// This is primarily useful for programs like build tools, because they will
+/// typically expect, as an example, the compiler or Haxelib to be available.
+/// Alongside this, all standard `stdio` streams are inherited for live input
+/// and output.
 pub fn haxe_exec(args: Vec<String>, config: Config, prog: Option<String>) -> Result<Output, Error> {
     match config.0.get_path_installed() {
         Ok(buf) => {
